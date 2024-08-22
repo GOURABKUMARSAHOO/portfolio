6ec0bdea47f642f51bbd89da9107010d67f663a5
Gourab Kumar Sahoo <152180227+GOURABKUMARSAHOO@users.noreply.github.com>
2024-08-22 14:36:38 +0530

    adding files to git hub

---
+++ script.js
@@ -0,0 +1,40 @@
+// toggle icon navbar
+let nemuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+// scroll section active
+let sections= document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+window.onscroll = ()=>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top>= offset && top < offset + height){
+            navlinks.forEach.apply(links=>{
+                links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+           });
+        };
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY>100);
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+ScrollReveal({
+    distance:'80ox',
+    duration:2000,
+    delay:200,
+
+})
+ScrollReveal().reveal('.home-content,heading', {origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container , .portfolio-box,.contact form', {origin:'bottom'});
+ScrollReveal().reveal('.home-contact h1,.about-img', {origin:'left'}); 
+ScrollReveal().reveal('.home-contact p,.about-content', {origin:'right'});
+
+
